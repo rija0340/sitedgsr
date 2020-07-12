a55ef373ad115a2bfd218aedd749a2a0c68acde5
raberia <rakotoarinelinarija@gmail.com>
2020-07-12 14:45:17 +0300

    design tarifs accordion fait

---
+++ public/js/tarifs.js
@@ -0,0 +1,45 @@
+
+
+
+$(document).ready(function() {
+
+
+	$(".btn_tarif").on('click', function( e ){
+
+		//recuperaton de l'element cliqué (unique car chacun a son href)
+		var $a = $(this);
+
+		//recuperation valeur attribut href
+		var url = $a.attr('href');
+
+		//recuperer dans id la valeur sans #
+		var id = url.slice(1, url.length);
+
+		//recuperer valeur attribut aria-expended pour tester si l'accordion est développé ou pas
+		var test = $a.attr('aria-expanded');
+		// console.log(test);
+		// console.log("id est :"  + id+"_icone");
+
+
+		// console.log($("#"+id+"_icone"));
+		
+
+		if (test == "true" ) {
+
+
+			$("#"+id+"_icone").removeClass("fa-toggle-up");
+			$("#"+id+"_icone").addClass("fa-toggle-down");
+
+		}else{
+
+
+			$("#"+id+"_icone").removeClass("fa-toggle-down");
+			$("#"+id+"_icone").addClass("fa-toggle-up");
+
+		}
+
+
+	});
+
+
+});
